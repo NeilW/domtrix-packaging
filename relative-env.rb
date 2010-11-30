0484edf1e964f7b8337b6993996acc57a1eb70d0
Neil Wilson <neil@aldur.co.uk>
2010-11-30 17:06:59 +0000

    change data directory to 'domtrix-lb'

--- relative-env.rb
+++ relative-env.rb
@@ -13,5 +13,5 @@ end
 rootdir = File.expand_path(File.join(File.dirname(__FILE__)))
 $: << File.join(rootdir,"lib")
 ENV['PATH']+=":#{File.join(rootdir,'bin')}"
-DATADIR = File.join(rootdir, 'data/domtrix_lb')
+DATADIR = File.join(rootdir, 'data/domtrix-lb')
 CONFDIR = File.join(rootdir, 'conf')
