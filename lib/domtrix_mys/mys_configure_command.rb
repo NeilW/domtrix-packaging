ed3a42f03bb9d627454c9641880a1a1ee2837a8f
Neil Wilson <neil@aldur.co.uk>
2014-01-10 14:22:12 +0000

    mys_configure_updates
      - add support for changing ftp user and password.
      - add support for updating admin password.
      - add support for updating git archive.

--- lib/domtrix_mys/mys_configure_command.rb
+++ lib/domtrix_mys/mys_configure_command.rb
@@ -15,33 +15,64 @@ private
     true
   end
 
+  def runtime_yaml_file
+    "/etc/brightbox/hiera/runtime.yaml"
+  end
+
+  def initial_yaml_hash
+    @initial_yaml ||= YAML.load_file(runtime_yaml_file)
+  rescue SystemCallError => e
+    Syslog.debug("#{self.class.name}: YAML load failure - #{e.message}")
+    @initial_yaml = {}
+  end
+
   def admin_password
     @data[:admin_password]
   end
-
-  def puppet_config_hash
-    {
-      "mys_service::admin_password" => admin_password
-    }
+  
+  def ftp_login
+    @data[:ftp_user]
   end
 
-  def write_mysql_puppet_config
+  def ftp_password
+    @data[:ftp_password]
+  end
+
+  def update_flag
+    if @data[:update]
+      "--update"
+    else
+      ""
+    end
+  end
+      
+
+  def build_puppet_config_hash
+    new_hash = {}
+    new_hash["mys_service::admin_password"] = admin_password if admin_password
+    new_hash["domtrix::ftplogin"] = ftp_login if ftp_login
+    new_hash["domtrix::ftppassword"] = ftp_password if ftp_password
+    initial_yaml_hash.merge new_hash
+  end
+
+  def write_mysql_puppet_config(puppet_hash)
     Syslog.debug "Updating Mysql hiera runtime config"
-    File.open("/etc/brightbox/hiera/runtime.yaml",
+    File.open(runtime_yaml_file,
       File::CREAT|File::TRUNC|File::WRONLY, 0600) do |f|
-      YAML.dump(puppet_config_hash, f)
+      YAML.dump(puppet_hash, f)
     end
     Syslog.debug "Updated."
   end
 
   def start_puppet_run
     Syslog.debug "Starting MySQL puppet reconfigure"
-    run("puppet-git-reapply", "Puppet manifests reapplied", "Puppet run failure")
+    run("puppet-git-reapply #{update_flag}", "Puppet manifests reapplied #{update_flag}", "Puppet run failure")
     Syslog.debug "Completed"
   end
 
   def data_action
-    write_mysql_puppet_config if admin_password
+    temp_hash = build_puppet_config_hash
+    write_mysql_puppet_config(build_puppet_config_hash)
     start_puppet_run
     @state="completed"
   end
